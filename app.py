42074d73f5cd6f83b6609e095ae39a15739c8547
winbigcoms <bigcoms@imform.co.kr>
2023-12-04 13:51:52 +0900

    set excel compare

---
+++ app.py
@@ -0,0 +1,20 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI()
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, query_param: str = None):
+    return {"item_id": item_id, "query_param": query_param}
---
+++ utill/excel.py
@@ -0,0 +1,16 @@
+from types.excel import RequestCompareExcel
+
+def compare_selected_rows_columns(df1, df2, start_row, end_row, columns_to_compare):
+    # 선택한 행과 열만 추출
+    df1_subset = df1.iloc[start_row:end_row, :][columns_to_compare]
+    df2_subset = df2.iloc[start_row:end_row, :][columns_to_compare]
+
+    # 두 데이터프레임 비교
+    diff_df = df1_subset.compare(df2_subset)
+
+    # 차이가 있는 경우 JSON 형태로 반환
+    if not diff_df.empty:
+        return diff_df.to_json(orient='split')
+    else:
+        return {"message": "선택한 행과 열에서의 두 시트는 동일합니다."}
+    
