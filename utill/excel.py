8e26ff2f83b046f0169c6449084f5b104e219177
winbigcoms <bigcoms@imform.co.kr>
2023-12-12 14:38:40 +0900

    compare excel

--- utill/excel.py
+++ utill/excel.py
@@ -13,8 +13,8 @@ async def find_different_values(file1:UploadFile, file2:UploadFile ):
         file_object.write(file2.file.read())
 
     # 엑셀 파일 읽기
-    df1 = pd.read_excel(file1_path, header=None)
-    df2 = pd.read_excel(file2_path, header=None)
+    df1 = pd.read_excel(file1_path, header=None, engine="openpyxl")
+    df2 = pd.read_excel(file2_path, header=None, engine="openpyxl")
     
     # 이름을 기준으로 두 데이터프레임을 병합
     merged_df = pd.merge(df1, df2, left_on=0, right_on=0,suffixes=('_left','_right'))
