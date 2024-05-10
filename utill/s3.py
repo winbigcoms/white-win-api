702422f88bdc60d50d1cf8738a58bf9a8d6b19ca
bigcoms <bigcoms6290@gmail.com>
2024-05-10 16:17:32 +0900

    upload img

--- utill/s3.py
+++ utill/s3.py
@@ -2,6 +2,8 @@ import os
 import boto3
 from dotenv import load_dotenv
 from fastapi import UploadFile
+import requests
+from io import BytesIO
 
 load_dotenv()
 
@@ -16,10 +18,15 @@ s3 = boto3.client(
 
 def upload_to_s3(file:UploadFile, uploader:str):
     filename = file.filename
+    response = requests.get(file.uri)
+    if response.status_code != 200:
+        raise HTTPException(status_code=400, detail="Failed to download image from URI")
+
+    image_data = BytesIO(response.content)
     s3.upload_fileobj(
-        file.file,
+        image_data,
         'with-you',
-        f"{uploader}/{file.file}",
+        f"{uploader}/{file.filename}",
     )
     return f"https://with-you.s3.ap-northeast-2.amazonaws.com/{uploader}/{filename}"
     
